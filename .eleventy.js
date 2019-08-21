b401a4366538d22467b3d404a732b91b9f753f04
Glitch (first-end) <none>
2019-08-21 21:20:00 +0000

    💳👸 Checkpoint ./README.md:293015/383 ./src/index.html:293015/22 ./src/index.md:293015/488 ./.eleventy.js:293015/81 ./src/_includes/layouts/default.njk:293015/230 ./src/_includes/layouts/style.css:293015/1294 ./css/style.css:293015/245

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,9 @@
 // docs: https://www.11ty.io/docs/config/
-
 module.exports = function(eleventyConfig) {
-  
   // eleventyConfig.addFilter( "myFilter", function() {});
- 
+  eleventyConfig.addPassthroughCopy('css');
   return {
+    passthroughFileCopy: true,
     dir: {
       input: "src",
       output: "dist"
---
+++ src/index.md
@@ -0,0 +1,8 @@
+---
+layout: layouts/default.njk
+pageTitle: Home
+---
+# hello world.
+
+Remember live journal and webrings and text? This is like that. 
+Use the Glitch communities to add people to your web ring. Clone this template 
