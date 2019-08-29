d5d232c5bc93a253787cbe5848e4758f42ca3f45
Glitch (veil-look) <none>
2019-08-29 15:19:21 +0000

    🌾🚃 Checkpoint ./.eleventy.js:234427/1025

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,15 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 // docs: https://www.11ty.io/docs/config/
 module.exports = function(eleventyConfig) {
   // eleventyConfig.addFilter( "myFilter", function() {});
-  eleventyConfig.addPassthroughCopy('css');
-  eleventyConfig.addCollection('posts', collection => {
-    return [...collection.getFilteredByGlob('./src/**/*.md')]
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addCollection("posts", collection => {
+    return [...collection.getFilteredByGlob("./src/**/*.md")]
       .filter(p => !p.data.draft)
       .reverse();
   });
-  
+
   return {
     passthroughFileCopy: true,
     dir: {
